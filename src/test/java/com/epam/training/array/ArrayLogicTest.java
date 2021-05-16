6d3a6e14a6ba27b28a2f0c3f7fad0417e0a5da25
TatsianaYemialyanava <tatsiana.yemialyanava@gmail.com>
2021-05-16 12:26:59 +0300

    added comments in tests

--- src/test/java/com/epam/training/array/ArrayLogicTest.java
+++ src/test/java/com/epam/training/array/ArrayLogicTest.java
@@ -7,109 +7,145 @@ public class ArrayLogicTest {
 
 	@Test
 	public void testExistsElement(){
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 4, 6, 8, 12, 12, 18, 20, 24, 27, 30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		int element = 8;
 		Array array = new Array(firstArray);
+		//when
 		boolean result = arrayLogic.exists(array, element);
+		//then(post-conditions)
 		Assert.assertTrue("Expected true", result);
 	}
 	
 	@Test
 	public void testNonExistsElement(){
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 4, 6, 8, 12, 12, 18, 20, 24, 27, 30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		int element = 1000000;
 		Array array = new Array(firstArray);
+		//when
 		boolean result = arrayLogic.exists(array, element);
+		//then(post-conditions)
 		Assert.assertFalse("Expected false", result);
 	}
 	
 	@Test
 	public void testExistsTwoSameElements(){
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 4, 8, 6, 12, 12,  18, 20, 24, 27, 30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		int element = 12;
 		Array array = new Array(firstArray);
+		//when
 		boolean result = arrayLogic.exists(array, element);
+		//then(post-conditions)
 		Assert.assertTrue("Expected true", result);
 	}
 	
 	@Test
     public void testIsPrimeNumberPositive() {
+		//given(pre-conditions)
     	ArrayLogic arrayLogic = new ArrayLogic();
+    	//when
     	boolean result = arrayLogic.isPrimeNumber(3);
+    	//then(post-conditions)
     	Assert.assertTrue("Expected true", result);
     }
 	
 	@Test
     public void testIsPrimeNumberNegative() {
+		//given(pre-conditions)
     	ArrayLogic arrayLogic = new ArrayLogic();
+    	//when
     	boolean result = arrayLogic.isPrimeNumber(6);
+    	//then(post-conditions)
     	Assert.assertFalse("Expected false", result);
     }
 
 	@Test
     public void testFindPrimeNumbers() {
+		//given(pre-conditions)
     	int[] firstArray = new int[] {2, 3, 7, 8, 12, 12, 18, 20, 24, 27, 30};
     	ArrayLogic arrayLogic = new ArrayLogic();
     	Array array = new Array(firstArray);
+    	//when
     	Array resultArray = arrayLogic.findPrimeNumbers(array);
     	int[] expectedArray = {2, 3, 7};
+    	//then(post-conditions)
 		Assert.assertArrayEquals(expectedArray, resultArray.getItems());
     }
 	
 	@Test
 	public void testSearchMinPositiveNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 3, 7, 8, 12, 12, 18, 20, 24, 27, 30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int resultInt =arrayLogic.searchMin(array);
+    	//then(post-conditions)
     	Assert.assertEquals(2, resultInt);
 	}
 	
 	@Test
 	public void testSearchMinNegativeNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[] {-2, -3, -7, -8, -12, -12, -18, -20, -24, -27, -30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int resultInt =arrayLogic.searchMin(array);
+    	//then(post-conditions)
     	Assert.assertEquals(-30, resultInt);
 	}
 	
 	@Test
 	public void testSearchMinInArrayWithSameNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 2, 2, 2, 2};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int resultInt =arrayLogic.searchMin(array);
+    	//then(post-conditions)
     	Assert.assertEquals(2, resultInt);
 	}
 	
 	@Test
 	public void testSearchMaxPositiveNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[] {2, 3, 7, 8, 12, 12, 18, 20, 24, 27, 30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int result =arrayLogic.searchMax(array);
+    	//then(post-conditions)
     	Assert.assertEquals(30, result);
 	}
 	
 	@Test
 	public void testSearchMaxNegativeNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[]  {-2, -3, -7, -8, -12, -12, -18, -20, -24, -27, -30};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int result =arrayLogic.searchMax(array);
+    	//then(post-conditions)
     	Assert.assertEquals(-2, result);
 	}
 	
 	@Test
 	public void testSearchMaxInArrayWithSameNumber () {
+		//given(pre-conditions)
 		int[] firstArray = new int[]  {2, 2, 2, 2, 2};
 		ArrayLogic arrayLogic = new ArrayLogic();
 		Array array = new Array(firstArray);
+		//when
     	int result =arrayLogic.searchMax(array);
+    	//then(post-conditions)
     	Assert.assertEquals(2, result);
 	}
 }
--- src/test/java/com/epam/training/array/ArraySorterTest.java
+++ src/test/java/com/epam/training/array/ArraySorterTest.java
@@ -7,9 +7,11 @@ public class ArraySorterTest {
 
     @Test
     public void testSortShouldSortWhenPositiveNumbers(){
+    	//given(pre-conditions)
     	int[] firstArray = new int[] {8, 43, 27, 68, 112, 7612, 18, 320, 224, 27, 130};
     	ArraySorter arraySorter = new ArraySorter();
     	Array arrayForSort = new Array(firstArray);
+    	//when
     	Array resultArray = arraySorter.sort(arrayForSort);
     	int[] expectedArray = {8, 18, 27, 27, 43, 68, 112, 130, 224, 320, 7612};
     	Assert.assertArrayEquals(expectedArray, resultArray.getItems());
@@ -18,30 +20,39 @@ public class ArraySorterTest {
     
     @Test
     public void testSortShouldSortWhenNegativeNumbers(){
+    	//given(pre-conditions)
     	int[] firstArray = new int[] {-8, -43, -27, -68, -112, -7612, -18, -320, -224, -27, -130};
     	ArraySorter arraySorter = new ArraySorter();
     	Array arrayForSort = new Array(firstArray);
+    	//when
     	Array resultArray = arraySorter.sort(arrayForSort);
+    	//then(post-conditions)
     	int[] expectedArray = {-7612, -320, -224, -130, -112, -68, -43, -27, -27, -18, -8};
     	Assert.assertArrayEquals(expectedArray, resultArray.getItems());
     }
     
     @Test
     public void testSortShouldSortWhenSameNumbers(){
+    	//given(pre-conditions)
     	int[] firstArray = new int[] {8, 8, 8, 8, 8};
     	ArraySorter arraySorter = new ArraySorter();
     	Array arrayForSort = new Array(firstArray);
+    	//when
     	Array resultArray = arraySorter.sort(arrayForSort);
+    	//then(post-conditions)
     	int[] expectedArray = {8, 8, 8, 8, 8};
     	Assert.assertArrayEquals(expectedArray, resultArray.getItems());
     }
     
     @Test
     public void testSortShouldSortWhenEmptyArrays(){
+    	//given(pre-conditions)
     	int[] firstArray = new int[] {};
     	ArraySorter arraySorter = new ArraySorter();
     	Array arrayForSort = new Array(firstArray);
+    	//when
     	Array resultArray = arraySorter.sort(arrayForSort);
+    	//then(post-conditions)
     	int[] expectedArray = {};
     	Assert.assertArrayEquals(expectedArray, resultArray.getItems());
     }
